14b6c85af54026a5adc62d5ec9223381093e5a50
Sven Schumann <s.schumann@tarent.de>
2014-06-24 15:02:52 +0200

    remove unused import

--- src/main/java/org/osiam/addons/administration/config/AdminAccessDecisionManager.java
+++ src/main/java/org/osiam/addons/administration/config/AdminAccessDecisionManager.java
@@ -8,7 +8,6 @@ import org.osiam.addons.administration.model.SessionData;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
