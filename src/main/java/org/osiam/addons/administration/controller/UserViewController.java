0674a443908aee969280fcc196480d7e6a1b0d27
Sven Schumann <s.schumann@tarent.de>
2014-06-26 11:09:27 +0200

    add handler-method for filter action requests

--- src/main/java/org/osiam/addons/administration/controller/UserViewController.java
+++ src/main/java/org/osiam/addons/administration/controller/UserViewController.java
@@ -1,5 +1,8 @@
 package org.osiam.addons.administration.controller;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.inject.Inject;
 
 import org.osiam.addons.administration.service.UserService;
@@ -7,6 +10,7 @@ import org.osiam.resources.scim.SCIMSearchResult;
 import org.osiam.resources.scim.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +24,7 @@ public class UserViewController {
 
     public static final String CONTROLLER_PATH = AdminController.CONTROLLER_PATH + "/user/list";
     
+    public static final String REQUEST_PARAMETER_ACTION = "action";
     public static final String REQUEST_PARAMETER_QUERY = "query";
     public static final String REQUEST_PARAMETER_LIMIT = "limit";
     public static final String REQUEST_PARAMETER_OFFSET = "offset";
@@ -50,5 +55,37 @@ public class UserViewController {
         modelAndView.addObject(MODEL_USER_LIST, userList);
         
         return modelAndView;
+    }
+    
+    @RequestMapping(params = REQUEST_PARAMETER_ACTION + "=filter")
+    public String handleFilterAction(
+            @RequestParam Map<String, String> allParameters){
+        
+        String filterQuery = buildFilterQuery(allParameters);
+        String urlQuery = REQUEST_PARAMETER_QUERY + "=" + filterQuery;
+        
+        return "redirect:" + CONTROLLER_PATH + "?" + urlQuery;
+    }
+    
+    protected String buildFilterQuery(Map<String, String> allRequestParams) {
+        StringBuilder filterQuery = new StringBuilder();
+        
+        for(Entry<String, String> param : allRequestParams.entrySet()){
+            if(param.getKey().startsWith("query.")){
+                final String queryField = param.getKey().replaceAll("^query\\.", "");
+                final String queryFieldValue = param.getValue();
+                
+                if(filterQuery.length() > 0){
+                    filterQuery.append(" AND ");
+                }
+                
+                filterQuery.append(queryField);
+                filterQuery.append(" sw = \"");
+                filterQuery.append(queryFieldValue);
+                filterQuery.append("\"");
+            }
+        }
+        
+        return filterQuery.toString();
     }
 }
