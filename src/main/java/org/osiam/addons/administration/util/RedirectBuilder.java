bda5ba68626d8e570e3e73f4f1ddf517b6a5e09c
Sven Schumann <s.schumann@tarent.de>
2014-06-30 13:21:43 +0200

    the RedirectBuilder can now handle single parameters

--- src/main/java/org/osiam/addons/administration/util/RedirectBuilder.java
+++ src/main/java/org/osiam/addons/administration/util/RedirectBuilder.java
@@ -1,5 +1,11 @@
 package org.osiam.addons.administration.util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * This class is responsible for creating an redirect string that can be returned by Spring-Request-Handler.
  */
@@ -7,6 +13,7 @@ public class RedirectBuilder {
     private String destination;
     private String path;
     private String query;
+    private Map<String, Object> parameters = new HashMap<String, Object>();
 
     /**
      * Set the url-path of the redirect destination.
@@ -22,7 +29,8 @@ public class RedirectBuilder {
     }
 
     /**
-     * Set the url-query of the redirect destination.
+     * Set the url-query of the redirect destination. 
+     * This query should be encoded before!
      * 
      * @param query
      *        The query of the url.
@@ -48,6 +56,22 @@ public class RedirectBuilder {
     }
 
     /**
+     * Add a parameter which will be append at the url query.
+     * The value of the parameter will be encode automatically.
+     * 
+     * @param key
+     *        The name of the parameter.
+     * @param value
+     *        The value of the parameter.
+     * @return this
+     */
+    public RedirectBuilder addParameter(String key, Object value) {
+        this.parameters.put(key, value);
+
+        return this;
+    }
+
+    /**
      * Return the redirect string. That can be used for spring-request-handler.
      * 
      * @return The redirect string.
@@ -66,9 +90,37 @@ public class RedirectBuilder {
                 redirect.append("?");
                 redirect.append(query);
             }
+
+            if (!parameters.isEmpty()) {
+                if (query == null) {
+                    redirect.append("?");
+                } else {
+                    redirect.append("&");
+                }
+
+                boolean isFirst = true;
+                for (Entry<String, Object> param : parameters.entrySet()) {
+                    if (!isFirst) {
+                        redirect.append("&");
+                    }
+
+                    redirect.append(param.getKey());
+                    if (param.getValue() != null) {
+                        redirect.append("=");
+
+                        try {
+                            String encodedValue = URLEncoder.encode(param.getValue().toString(), "UTF-8");
+                            redirect.append(encodedValue);
+                        } catch (UnsupportedEncodingException e) {
+                            throw new IllegalStateException(e);
+                        }
+                    }
+
+                    isFirst = false;
+                }
+            }
         }
 
         return redirect.toString();
     }
-
 }
