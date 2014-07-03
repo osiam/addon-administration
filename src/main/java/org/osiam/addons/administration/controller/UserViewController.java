cdd7b88ec9f313ee1849b72a3959e4a167183bdd
Sven Schumann <s.schumann@tarent.de>
2014-07-03 10:17:11 +0200

    use POST-Method to submit the forms (we must include CSRF-Token!)

--- src/main/java/org/osiam/addons/administration/controller/UserViewController.java
+++ src/main/java/org/osiam/addons/administration/controller/UserViewController.java
@@ -15,6 +15,7 @@ import org.osiam.resources.scim.SCIMSearchResult;
 import org.osiam.resources.scim.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -47,7 +48,7 @@ public class UserViewController {
     @Inject
     private UserlistSession session;
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView handleList(
             @RequestParam(value = REQUEST_PARAMETER_QUERY, required = false) String query,
             @RequestParam(value = REQUEST_PARAMETER_LIMIT, required = false) Integer limit,
