06b4cd05f66fe25a3b98b4f08e9c7f2405b5b50b
sweiz <s.weiz@tarent.de>
2014-07-14 14:46:47 +0200

    Enabled UTF-8 Encoding

--- src/main/java/org/osiam/addons/administration/config/SecurityConfig.java
+++ src/main/java/org/osiam/addons/administration/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * This class contains the configuration about the web security.
@@ -19,7 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private AdminAccessDecisionManager decisionManager;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception { //NOSONAR the authorizeRequests() throws it and can't be omitted
+    protected void configure(HttpSecurity http) throws Exception { // NOSONAR the authorizeRequests() throws it and can't be omitted
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+
+        http.addFilterBefore(filter, CsrfFilter.class);
         http.authorizeRequests()
                 .accessDecisionManager(decisionManager)
                 .antMatchers(AdminController.CONTROLLER_PATH + "/**")
