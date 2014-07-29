c8c457ba8fb04f9566fc84ac082f87b484f5584e
Sven Schumann <s.schumann@tarent.de>
2014-07-29 15:39:44 +0200

    redirect to login page if user is not logged in (or accesstoken expired)

---
+++ src/main/java/org/osiam/addons/administration/controller/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package org.osiam.addons.administration.controller;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osiam.addons.administration.model.session.GeneralSessionData;
+import org.osiam.addons.administration.util.RedirectBuilder;
+import org.osiam.client.exception.UnauthorizedException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * This class is responsible for handle all exception that could be thrown by any controller-handler.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler implements AccessDeniedHandler {
+
+    @Inject
+    private GeneralSessionData session;
+    
+    
+    @ExceptionHandler(UnauthorizedException.class)
+    public String handleUnauthorized() {
+        session.setAccesstoken(null);
+        
+        return new RedirectBuilder().setDestination(LoginController.CONTROLLER_PATH).build();
+    }
+    
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+            AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        
+        response.sendRedirect(LoginController.CONTROLLER_PATH);
+    }
+}
