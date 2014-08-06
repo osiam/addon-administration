d9ac37909cf1a8be54bf9339c62a68f3cfeae219
Max Marche <m.marche@tarent.de>
2014-08-06 18:12:23 +0200

    add scimuser fields to command object

--- src/main/java/org/osiam/addons/administration/model/command/UpdateUserCommand.java
+++ src/main/java/org/osiam/addons/administration/model/command/UpdateUserCommand.java
@@ -1,6 +1,7 @@
 package org.osiam.addons.administration.model.command;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.osiam.resources.helper.SCIMHelper;
@@ -19,12 +20,35 @@ public class UpdateUserCommand {
     private User user;
 
     private String id;
-    
+    private Boolean active;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String title;
+    @NotNull
+    private String displayname;
+    @NotNull
+    private String nickname;
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]{2}$")
+    private String preferredlanguage;
+    @NotNull
+    @Pattern(regexp = ".\\@.*\\..*")
+    private String email;
+    @NotNull
+    @Pattern(regexp = "^[a-z]{2}_[A-Z]{2}&")
+    private String locale;
+    @NotNull
+    @Pattern(regexp = "www\\..*\\..*")
+    private String profileurl;
+    @NotNull
+    @Pattern(regexp = ".*\\/.*")
+    private String timezone;
     @NotNull
     @Size(min = 1)
-    private String firstName;
-    private String lastName;
-    private String email;
+    private String username;
 
     /**
      * Creates a new UpdateUserCommand based on the given {@link User}.
@@ -35,6 +59,17 @@ public class UpdateUserCommand {
     public UpdateUserCommand(User user) {
         this.user = user;
         setId(user.getId());
+
+        setActive(user.isActive());
+        setTitle(user.getTitle());
+        setDisplayname(user.getDisplayName());
+        setNickname(user.getNickName());
+        setPreferredlanguage(user.getPreferredLanguage());
+        setLocale(user.getLocale());
+        setProfileurl(user.getProfileUrl());
+        setTimezone(user.getTimezone());
+        setUsername(user.getUserName());
+
         if (user.getName() != null) {
             setFirstName(user.getName().getGivenName());
             setLastName(user.getName().getFamilyName());
@@ -109,6 +144,186 @@ public class UpdateUserCommand {
     }
 
     /**
+     * Returns the displayname.
+     * 
+     * @return the the displayname
+     */
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    /**
+     * Sets the displayname.
+     * 
+     * @param displayname
+     *        the displayname to set
+     */
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    /**
+     * Returns the locale.
+     * 
+     * @return the the locale
+     */
+    public String getLocale() {
+        return locale;
+    }
+
+    /**
+     * Sets the locale.
+     * 
+     * @param locale
+     *        the locale to set
+     */
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    /**
+     * Returns the nickname.
+     * 
+     * @return the the nickname
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * Sets the nickname.
+     * 
+     * @param nickname
+     *        the nickname to set
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * Returns the preferredlanguage.
+     * 
+     * @return the the preferredlanguage
+     */
+    public String getPreferredlanguage() {
+        return preferredlanguage;
+    }
+
+    /**
+     * Sets the preferredlanguage.
+     * 
+     * @param preferredlanguage
+     *        the preferredlanguage to set
+     */
+    public void setPreferredlanguage(String preferredlanguage) {
+        this.preferredlanguage = preferredlanguage;
+    }
+
+    /**
+     * Returns the profileurl.
+     * 
+     * @return the the profileurl
+     */
+    public String getProfileurl() {
+        return profileurl;
+    }
+
+    /**
+     * Sets the profileurl.
+     * 
+     * @param profileurl
+     *        the profileurl to set
+     */
+    public void setProfileurl(String profileurl) {
+        this.profileurl = profileurl;
+    }
+
+    /**
+     * Returns the timezone.
+     * 
+     * @return the the timezone
+     */
+    public String getTimezone() {
+        return timezone;
+    }
+
+    /**
+     * Sets the timezone.
+     * 
+     * @param timezone
+     *        the timezone to set
+     */
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    /**
+     * Returns the title.
+     * 
+     * @return the the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the title.
+     * 
+     * @param title
+     *        the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Returns the username.
+     * 
+     * @return the the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the username.
+     * 
+     * @param username
+     *        the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Returns the active.
+     * 
+     * @return the the active
+     */
+    public Boolean getActive() {
+        return active;
+    }
+
+    /**
+     * Sets the active.
+     * 
+     * @param active
+     *        the active to set
+     */
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * Returns the user.
+     * 
+     * @return the the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
      * Returns the user ID.
      * 
      * @return the user ID
@@ -145,6 +360,16 @@ public class UpdateUserCommand {
     public UpdateUser getAsUpdateUser() {
         Name name = new Name.Builder().setGivenName(getFirstName()).setFamilyName(getLastName()).build();
         UpdateUser.Builder builder = new UpdateUser.Builder().updateName(name);
+
+        builder.updateActive(getActive());
+        builder.updateTitle(getTitle());
+        builder.updateDisplayName(getDisplayname());
+        builder.updateNickName(getNickname());
+        builder.updatePreferredLanguage(getPreferredlanguage());
+        builder.updateLocale(getLocale());
+        builder.updateProfileUrl(getProfileurl());
+        builder.updateTimezone(getTimezone());
+        builder.updateUserName(getUsername());
 
         Optional<Email> previousEmail = SCIMHelper.getPrimaryOrFirstEmail(user);
         if (previousEmail.isPresent()) {
