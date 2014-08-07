9275ec59596ba8be9a32ee0f6e7475affbe0f888
Sven Schumann <s.schumann@tarent.de>
2014-08-07 16:02:13 +0200

    implements javascript mechanism to add a new command email dynamically

---
+++ src/main/resources/js/multivalue.js
@@ -0,0 +1,36 @@
+$(function(){
+	
+	$("#button-add-email").click(function(){
+		var allEmailBlocks = $("[id^='email-block-']");
+		var displayedEmailCount = allEmailBlocks.size();
+		var lastEmailBlock = allEmailBlocks.last();
+		
+		var myIndex = displayedEmailCount;
+		var newBlock = cloneBlock('#email-template', 'email-block-' + myIndex, myIndex);
+		
+		lastEmailBlock.after(newBlock);
+		newBlock.show();
+	});
+	
+	function cloneBlock(searchString, newId, newIndex){
+		var templateBlock = $(searchString);
+		var newBlock = templateBlock.clone();
+
+		newBlock.attr('id', newId);
+		newBlock.find('*').each(function(e, element) {
+			$.each(this.attributes, function(a, attr){
+				var name = attr.name;
+				var value = attr.value;
+				
+				if(value.indexOf("__ID__") > -1){
+					var newValue = value.replace(/__ID__/g, newIndex);
+					$(element).attr(name, newValue);
+				}
+				
+			});
+		});
+		
+		return newBlock;
+	};
+	
+});
