623cad70de0921ee9dd3c94067accb2f979910a3
Max Marche <m.marche@tarent.de>
2014-08-07 17:34:53 +0200

    Create Update User Js Dialog

--- src/main/resources/js/custom-dialogs.js
+++ src/main/resources/js/custom-dialogs.js
@@ -1,55 +1,82 @@
-$(function () {
-    $("button[id^='action-button-deactivate-']").click(function () {
-        var button = $(this);
+$(function(){
+	$("#btnSaveChanges").click(function(){
+		console.log("Diolog event triggered");
+		var button = $(this);
+		
+		bootbox.dialog({
+			message : $('#dialog-updateUser-message').val(),
+			title : $('#dialog-title').val(),
+			buttons : {
+				success : {
+					label : $('#dialog-success').val(),
+					className : "btn-success",
+					callback : function() {
+						button.closest("form").submit();
+					}
+				},
+				danger : {
+					label : $('#dialog-abort').val(),
+					className : "btn-danger"
+				}
+			}
+		});
 
-        bootbox.dialog({
-            message: $('#dialog-deactivate-message').html(),
-            title: $('#dialog-title').val(),
-            buttons: {
-                success: {
-                    label: $('#dialog-success').val(),
-                    className: "btn-success",
-                    callback: function () {
-                        var sendMail = $('div[role="dialog"] #send-mail').is(':checked');
-
-                        button.parent().find('input[name="sendMail"]').val(sendMail);
-                        button.parent().submit();
-                    }
-                },
-                danger: {
-                    label: $('#dialog-abort').val(),
-                    className: "btn-danger"
-                }
-            }
-        });
-
-        return false;
-    });
-
-    $("button[id^='action-button-activate-']").click(function () {
-        var button = $(this);
-
-        bootbox.dialog({
-            message: $('#dialog-activate-message').html(),
-            title: $('#dialog-title').val(),
-            buttons: {
-                success: {
-                    label: $('#dialog-success').val(),
-                    className: "btn-success",
-                    callback: function () {
-                        var sendMail = $('div[role="dialog"] #send-mail').is(':checked');
-
-                        button.parent().find('input[name="sendMail"]').val(sendMail);
-                        button.parent().submit();
-                    }
-                },
-                danger: {
-                    label: $('#dialog-abort').val(),
-                    className: "btn-danger"
-                }
-            }
-        });
-
-        return false;
-    });
+		return false;
+	});
+	
+	
+	$("button[id^='action-button-deactivate-']").click(function(){
+		var button = $(this);
+		
+		bootbox.dialog({
+			message : $('#dialog-deactivate-message').html(),
+			title : $('#dialog-title').val(),
+			buttons : {
+				success : {
+					label : $('#dialog-success').val(),
+					className : "btn-success",
+					callback : function() {
+						var sendMail = $('div[role="dialog"] #send-mail').is(':checked');
+						
+						button.parent().find('input[name="sendMail"]').val(sendMail);
+						button.parent().submit();
+					}
+				},
+				danger : {
+					label : $('#dialog-abort').val(),
+					className : "btn-danger"
+				}
+			}
+		});
+		
+		return false;
+	});
+	
+	
+	$("button[id^='action-button-activate-']").click(function(){
+		var button = $(this);
+		
+		bootbox.dialog({
+			message : $('#dialog-activate-message').html(),
+			title : $('#dialog-title').val(),
+			buttons : {
+				success : {
+					label : $('#dialog-success').val(),
+					className : "btn-success",
+					callback : function() {
+						var sendMail = $('div[role="dialog"] #send-mail').is(':checked');
+						
+						button.parent().find('input[name="sendMail"]').val(sendMail);
+						button.parent().submit();
+					}
+				},
+				danger : {
+					label : $('#dialog-abort').val(),
+					className : "btn-danger"
+				}
+			}
+		});
+		
+		return false;
+	});
 });
