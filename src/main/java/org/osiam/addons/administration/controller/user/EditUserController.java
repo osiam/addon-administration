2a262f0ace32297873ec0ddb5874e73758649a55
Sven Schumann <s.schumann@tarent.de>
2014-08-08 16:30:01 +0200

    implements delete-meachanism for emails/phoneNumbers/im

--- src/main/java/org/osiam/addons/administration/controller/user/EditUserController.java
+++ src/main/java/org/osiam/addons/administration/controller/user/EditUserController.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.apache.log4j.Logger;
 import org.osiam.addons.administration.controller.AdminController;
 import org.osiam.addons.administration.controller.GenericController;
+import org.osiam.addons.administration.model.command.EmailCommand;
 import org.osiam.addons.administration.model.command.UpdateUserCommand;
 import org.osiam.addons.administration.service.UserService;
 import org.osiam.addons.administration.util.RedirectBuilder;
@@ -38,6 +39,9 @@ public class EditUserController extends GenericController {
 
     @Inject
     private UserService userService;
+    
+    @Inject
+    private Validator validator;
 
     @Inject
     private Validator validator;
