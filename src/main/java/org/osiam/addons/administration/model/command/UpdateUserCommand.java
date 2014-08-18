62b6e462add63a0d12fca902da0d6c835bd07813
Sven Schumann <s.schumann@tarent.de>
2014-08-18 17:32:15 +0200

    read and write extensions in command object and implements extension validation

--- src/main/java/org/osiam/addons/administration/model/command/UpdateUserCommand.java
+++ src/main/java/org/osiam/addons/administration/model/command/UpdateUserCommand.java
@@ -1,7 +1,7 @@
 package org.osiam.addons.administration.model.command;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
