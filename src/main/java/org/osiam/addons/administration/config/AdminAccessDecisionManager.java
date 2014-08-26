98eb582c7f10f265a2c99e0632abc3ab302b93e4
Max Marche <m.marche@tarent.de>
2014-08-26 17:12:41 +0200

    impl access to administration UI only for privileges users

--- src/main/java/org/osiam/addons/administration/config/AdminAccessDecisionManager.java
+++ src/main/java/org/osiam/addons/administration/config/AdminAccessDecisionManager.java
@@ -1,10 +1,20 @@
 package org.osiam.addons.administration.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import org.osiam.addons.administration.model.session.GeneralSessionData;
+import org.osiam.client.OsiamConnector;
+import org.osiam.client.query.QueryBuilder;
+import org.osiam.resources.scim.GroupRef;
+import org.osiam.resources.scim.SCIMSearchResult;
+import org.osiam.resources.scim.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
@@ -20,11 +30,46 @@ public class AdminAccessDecisionManager implements AccessDecisionManager {
     @Inject
     private GeneralSessionData session;
 
+    @Inject
+    private OsiamConnector connector;
+
+    @Value("${org.osiam.administration.adminGroups}")
+    private String[] adminGroups;
+
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) {
         // if there are no access-token in session
         if (session.getAccesstoken() == null) {
             throw new AccessDeniedException("There is no accesstoken in session!");
+        }
+
+        //groups.display eq "vr_bewerber" OR groups.display eq "vr_anwaerterStipendiat" OR groups.display eq "vr_stipendiat" OR groups.display eq "vr_anwaerterAltstipendiat"
+//        List<GroupRef> userGroups = connector.getCurrentUser(session.getAccesstoken()).getGroups();
+
+        StringBuilder queryString = new StringBuilder();
+        queryString.append("userName eq ");
+        queryString.append("\"");
+        queryString.append(session.getAccesstoken().getUserName());
+        queryString.append("\"");
+        if (adminGroups.length > 0) {
+            queryString.append(" AND ");
+        }
+
+        Iterator<String> groupIterator = Arrays.asList(adminGroups).iterator();
+        while (groupIterator.hasNext()) {
+            queryString.append("groups.display eq \"");
+            queryString.append(groupIterator.next());
+            queryString.append("\"");
+
+            if(groupIterator.hasNext()) {
+                queryString.append(" OR ");
+            }
+        }
+
+        SCIMSearchResult<User> a = connector.searchUsers(new QueryBuilder().filter(queryString.toString()).build(), session.getAccesstoken());
+
+        if(a.getTotalResults() != 1) {
+            throw new AccessDeniedException("No permissions to user admin gui");
         }
     }
 
