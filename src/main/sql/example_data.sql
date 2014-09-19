4ebbff4b5c44787d057b2b4ecdf9289bd40a7cf7
David Crome <d.crome@tarent.de>
2014-09-19 13:12:19 +0200

    [enhancement] prepared for distribution build
    - added sql file to import addon client
    - added porperties file needed for auto deployment

---
+++ src/main/sql/example_data.sql
@@ -0,0 +1,19 @@
+--
+-- Example Client, please sync to the attribute values in the addon-administration.properties.
+-- Please update all queries in this file to your needs!
+-- Have to be imported in the database of the auth-server, before you deploy the addon-administration!
+--
+INSERT INTO osiam_client (internal_id, accesstokenvalidityseconds, client_secret, expiry,
+id, implicit_approval, redirect_uri, refreshtokenvalidityseconds,
+validityinseconds)
+VALUES (1, 2342, 'super-secret', null, 'addon-administration-client', FALSE, 'http://localhost:8080/addon-administration', 4684, 1337);
+
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'GET');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'POST');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'PUT');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'PATCH');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'DELETE');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'authorization_code');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'refresh_token');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'password');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'client_credentials');
