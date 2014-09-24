4c3ad241f313be74e11d0e4d837476ec46478a0b
Sven Schumann <s.schumann@tarent.de>
2014-09-24 15:01:16 +0200

    implement master-checkbox functionality

--- src/main/resources/js/general.js
+++ src/main/resources/js/general.js
@@ -1,3 +1,12 @@
 $(function(){
 	$('.only-on-js').show();
+
+	$('.master-checkbox').change(function(){
+		var colIndex = $(this).parents('th, td').index();
+		var checked = this.checked;
+
+		var childBoxes = $(this).parents('table').find('td:nth-child(' + (colIndex + 1) + ') input[type="checkbox"]');
+
+		childBoxes.prop('checked', checked);
+	});
 });
