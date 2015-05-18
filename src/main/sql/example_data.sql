d076ce778888fefd30402e6efc5dc63d29808530
Thomas Krille <tkrille@gmail.com>
2015-05-18 06:38:53 +0200

    [fix] restore missing client

--- src/main/sql/example_data.sql
+++ src/main/sql/example_data.sql
@@ -1,8 +1,26 @@
 --
--- Example Client, please sync to the attribute values in the addon-administration.properties.
--- Please update all queries in this file to your needs!
--- Have to be imported in the database of the auth-server, before you deploy the addon-administration!
+-- Example Client, please sync to the attribute values in the
+-- addon-administration.properties. Have to be imported in the database of the
+-- auth-server, before you deploy the addon-administration!
 --
+
+INSERT INTO osiam_client (internal_id, accesstokenvalidityseconds, client_secret, expiry,
+                          id, implicit_approval, redirect_uri, refreshtokenvalidityseconds,
+                          validityinseconds)
+VALUES (20, 2342, 'super-secret', NULL,
+        'addon-administration-client', FALSE, 'http://localhost:8080/addon-administration', 4684,
+        1337);
+
+INSERT INTO osiam_client_scopes (id, scope) VALUES (20, 'GET');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (20, 'POST');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (20, 'PUT');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (20, 'PATCH');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (20, 'DELETE');
+INSERT INTO osiam_client_grants (id, grants) VALUES (20, 'authorization_code');
+INSERT INTO osiam_client_grants (id, grants) VALUES (20, 'refresh_token');
+INSERT INTO osiam_client_grants (id, grants) VALUES (20, 'password');
+INSERT INTO osiam_client_grants (id, grants) VALUES (20, 'client_credentials');
+
 -- resource server: example group 'admin' for the addon-administration
 
 INSERT INTO scim_meta (id, created, lastmodified, location, resourcetype, version)
