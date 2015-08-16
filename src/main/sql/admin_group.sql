5ca0f98a9c37fd81c6d388f4eb9bf9f392539b79
Thomas Krille <t.krille@tarent.de>
2015-08-16 22:36:21 +0200

    Fix column names in 'admin_group.sql'

--- src/main/sql/admin_group.sql
+++ src/main/sql/admin_group.sql
@@ -6,10 +6,10 @@
 INSERT INTO scim_meta (id, created, last_modified, location, resource_type, version)
 VALUES (2, LOCALTIMESTAMP, LOCALTIMESTAMP, NULL, 'Group', NULL);
 
-INSERT INTO scim_id (internal_id, external_id, id, meta_id)
+INSERT INTO scim_id (internal_id, external_id, id, meta)
 VALUES (2, NULL, 'cef9452e-00a9-4cec-a086-d171374aabef', 2);
 
-INSERT INTO scim_group (displayname, internal_id)
+INSERT INTO scim_group (display_name, internal_id)
 VALUES ('admin', 2);
 
 -- add default admin user to group
