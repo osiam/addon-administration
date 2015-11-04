a30307c81e6199da7cf5b25060de8c5a729c0e76
Thomas Krille <t.krille@tarent.de>
2015-11-04 10:34:12 +0100

    Restore support for OSIAM 2.x
    We only want to maintain a single version of the addon by now, so old
    behavior must be restored. This restores the deleted configuration
    properties to connect to OSIAM 2.x and adds support for the new legacy
    schema mode of the connector.

--- src/main/java/org/osiam/addons/administration/config/ConnectorBuilder.java
+++ src/main/java/org/osiam/addons/administration/config/ConnectorBuilder.java
@@ -1,6 +1,8 @@
 package org.osiam.addons.administration.config;
 
+import com.google.common.base.Strings;
 import org.osiam.client.OsiamConnector;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,19 +19,38 @@ public class ConnectorBuilder {
     @Value("${org.osiam.clientSecret}")
     private String clientSecret;
 
-    @Value("${org.osiam.endpoint}")
+    @Value("${org.osiam.authServerEndpoint:}")
+    private String authServerEndpoint;
+
+    @Value("${org.osiam.resourceServerEndpoint:}")
+    private String resourceServerEndpoint;
+
+    @Value("${org.osiam.endpoint:}")
     private String osiamEndpoint;
+
+    @Value("${org.osiam.connector.legacy-schemas:false}")
+    private boolean useLegacySchemas;
 
     @Value("${org.osiam.redirectUri}")
     private String redirectUri;
 
     @Bean
     public OsiamConnector build() {
-        return new OsiamConnector.Builder()
-                .withEndpoint(osiamEndpoint)
+        OsiamConnector.Builder builder = new OsiamConnector.Builder()
                 .setClientRedirectUri(redirectUri)
                 .setClientId(clientId)
-                .setClientSecret(clientSecret)
-                .build();
+                .setClientSecret(clientSecret);
+
+        if (!Strings.isNullOrEmpty(osiamEndpoint)) {
+            builder.withEndpoint(osiamEndpoint);
+        } else if (!Strings.isNullOrEmpty(authServerEndpoint) && !Strings.isNullOrEmpty(resourceServerEndpoint)) {
+            builder.setAuthServerEndpoint(authServerEndpoint)
+                    .setResourceServerEndpoint(resourceServerEndpoint)
+                    .withLegacySchemas(useLegacySchemas);
+        } else {
+            throw new BeanCreationException("Error creating OSIAM connector. No OSIAM endpoint set.");
+        }
+
+        return builder.build();
     }
 }
