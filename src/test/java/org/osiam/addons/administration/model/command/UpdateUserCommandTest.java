0d2cfca6912ee5fa6152da41db15e3262532049a
Thomas Krille <t.krille@tarent.de>
2016-06-04 17:13:57 +0200

    Switch from updating resources to replacing them
    OSIAM 3.0 removes support for updates via PATCH, so we have to use the
    PUT-based API from now on.

--- src/test/java/org/osiam/addons/administration/model/command/UpdateUserCommandTest.java
+++ src/test/java/org/osiam/addons/administration/model/command/UpdateUserCommandTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 import org.osiam.resources.scim.Extension;
-import org.osiam.resources.scim.UpdateUser;
 import org.osiam.resources.scim.User;
 
 public class UpdateUserCommandTest {
@@ -49,43 +48,5 @@ public class UpdateUserCommandTest {
         assertEquals(this.locale, command.getLocale());
         assertEquals(this.profileUrl, command.getProfileURL());
         assertEquals(this.timezone, command.getTimezone());
-    }
-
-    @Test
-    public void asUpdateUser() {
-        UpdateUserCommand command = new UpdateUserCommand(user, new ArrayList<Extension>(0));
-
-        String userName = "updatali";
-        Boolean isActive = true;
-        String title = "Weltraumpr\u00e4sidentin";
-        String displayName = "Hans";
-        String nickName = "MasterOfDisaster";
-        String preferredLanguage = "EN";
-        String locale = "df_GF";
-        String profileUrl = "myProfile.gibtsImmerNochNicht.com";
-        String timezone = "Mond/DieHelleSeite";
-
-        command.setUserName(userName);
-        command.setActive(isActive);
-        command.setTitle(title);
-        command.setDisplayName(displayName);
-        command.setNickName(nickName);
-        command.setPreferredLanguage(preferredLanguage);
-        command.setLocale(locale);
-        command.setProfileURL(profileUrl);
-        command.setTimezone(timezone);
-
-        UpdateUser updateUser = command.getAsUpdateUser();
-        User resultingUser = updateUser.getScimConformUpdateUser();
-
-        assertEquals(userName, resultingUser.getUserName());
-        assertEquals(isActive, resultingUser.isActive());
-        assertEquals(title, resultingUser.getTitle());
-        assertEquals(displayName, resultingUser.getDisplayName());
-        assertEquals(nickName, resultingUser.getNickName());
-        assertEquals(preferredLanguage, resultingUser.getPreferredLanguage());
-        assertEquals(locale, resultingUser.getLocale());
-        assertEquals(profileUrl, resultingUser.getProfileUrl());
-        assertEquals(timezone, resultingUser.getTimezone());
     }
 }
